e78f34ad16dd0ae80c6ea1fc04cbc57554255f0e
John Liu <33496334+jliu01298@users.noreply.github.com>
2019-04-08 21:37:40 -0400

    Added responses for not valid word choices

--- main.py
+++ main.py
@@ -38,12 +38,18 @@ def main(word):
     s = set()
     for i in l:
         s.add(i)
+    while word not in s:
+        word=input("Please choose a different word:\n")    
     b = findcontainers(word, s)
     ret = b[random.randrange(len(b))]
     ctr = ret.find(word)
     stop = ctr + len(word)
     noun = input("Give a singular noun associated with " + word + ":\n")
+    while len(noun)==0:
+            noun = input("Please give a valid noun:\n")
     verb = input("Give an action associated with " + ret + " (present tense):\n")
+    while len(verb)==0:
+            noun = input("Please give a valid verb:\n")
     ret = ret[0:ctr] + ret[ctr:stop].upper() + ret[stop:]
     print("How does a " + noun +  " " + verb + "?")
     print(ret)
