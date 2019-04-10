f55824b6f8462649f8baa71ffc35eb61c526a842
nadinejackson <njackson@stuy.edu>
2019-04-10 09:52:37 -0400

    fixed check for possibility

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
 import sys
 import random
-def wlen(filename):
-	f = open(filename, 'r')
-	str = f.read()
-	l = str.split('\n')
-	list = [0] * 26
-	for i in l:
-		list[len(i)]  += 1
-	list[0] = 0
-	return list
-
-def wordslength(num, filename):
-	f = open(filename, 'r')
-	str = f.read()
-	l = str.split('\n')
-	s = set()
-	for i in l:
-		if len(i) == num:
-			s.add(i)
-	return s
 
 def findcontainers(word, s):
         ret = []
         ctr = 0
         for palabra in s:
-            if word in palabra and palabra != word + "s" and palabra != word + "ing" and palabra != word and palabra != word + "ed" and palabra != word + "est" and palabra != word + "ish" and palabra != word + "er":
+            if word in palabra and palabra != word + "s" and palabra != word + "ing" and palabra != word and palabra != word + "ed" and palabra != word + "est" and palabra != word + "ish" and palabra != word + "er" and palabra != word + "ly" and palabra != word + "ness":
                 ret.append(palabra)
         return ret
 
@@ -38,9 +19,10 @@ def main(word):
     s = set()
     for i in l:
         s.add(i)
-    while word not in s:
-        word=input("Please choose a different word:\n")    
     b = findcontainers(word, s)
+    while len(b) == 0:
+        word = input("Please choose a different word:\n")
+        b = findcontainers(word, s)
     ret = b[random.randrange(len(b))]
     ctr = ret.find(word)
     stop = ctr + len(word)
